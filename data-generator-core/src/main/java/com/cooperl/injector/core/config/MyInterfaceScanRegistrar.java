3789578a5aab6176592809e6c4dc3b9bcded84de
LeJeanbono <jean.michel.lec@gmail.com>
2019-11-12 00:20:36 +0100

    Karate

--- data-generator-core/src/main/java/com/cooperl/injector/core/config/MyInterfaceScanRegistrar.java
+++ data-generator-core/src/main/java/com/cooperl/injector/core/config/MyInterfaceScanRegistrar.java
@@ -2,11 +2,13 @@ package com.cooperl.injector.core.config;
 
 import com.cooperl.injector.core.annotation.EnableDataGenerator;
 import com.cooperl.injector.core.annotation.TestData;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
 import org.springframework.core.env.Environment;
 import org.springframework.core.type.AnnotationMetadata;
--- data-generator-mongodb-e2e/src/main/java/com/example/e2e/E2EApplication.java
+++ data-generator-mongodb-e2e/src/main/java/com/example/e2e/E2EApplication.java
@@ -3,9 +3,11 @@ package com.example.e2e;
 import com.cooperl.injector.core.annotation.EnableDataGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@EnableDataGenerator(value = "")
+@EnableDataGenerator(value = "com.example.e2e")
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example.e2e", "com.cooperl.injector"})
 public class E2EApplication {
 
 	public static void main(String[] args) {
