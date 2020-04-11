e80f687bf54ccef96dd64a11221dc5a7039ca63f
LeJeanbono <jean.michel.lec@gmail.com>
2020-04-11 03:39:23 +0200

    Fix config

--- data-generator-core/src/main/java/com/cooperl/injector/core/config/DataGeneratorConfig.java
+++ data-generator-core/src/main/java/com/cooperl/injector/core/config/DataGeneratorConfig.java
@@ -1,13 +1,13 @@
 package com.cooperl.injector.core.config;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "data-generator")
 public class DataGeneratorConfig {
 
-    private Boolean pluralRessources = false;
+    @Value("${datagenerator.pluralRessources:false}")
+    private Boolean pluralRessources;
 
     public Boolean getPluralRessources() {
         return pluralRessources;
